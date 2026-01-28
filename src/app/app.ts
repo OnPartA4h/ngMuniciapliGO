ded7efb6684903c919292ce0414e5b1dea9da890
Adan Riasat <6194262@cegepmontpetit.ca>
2026-01-28 10:47:51 -0500

    FCT Login UI

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, CommonModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
 export class App {
+  showHeaderFooter = true;
+  private hiddenRoutes = ['/login', '/signup'];
 
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.showHeaderFooter = !this.hiddenRoutes.includes(event.urlAfterRedirects);
+      });
+  }
 }
