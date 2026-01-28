c718dc3fdfa9a658890077a2b52d412b76b9e7a9
Adan Riasat <6194262@cegepmontpetit.ca>
2026-01-28 15:13:54 -0500

    FCT Login logique

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
 
+  email: string = ""
+  password: string = ""
+
+  constructor(public authService: AuthService) {}
+
+  async login() {
+    await this.authService.login(this.email, this.password)
+  }
 }
---
+++ src/app/services/auth-service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  domain = "https://localhost:7288"
+  //domain = "https://municipaligo.onrender.com"
+
+  constructor(public http: HttpClient) {}
+
+  async login(email: string, password: string) {
+    let dto = {
+      email: email,
+      password: password
+    }
+
+    let x = await lastValueFrom(this.http.post<any>(this.domain + "/api/Auth/login", dto))
+    console.log(x);
+
+    let roles: string[] = x.user.roles
+
+    if (!roles.includes("Admin")) {
+      console.log("NOT ADMIN!!!!");
+      return
+    }
+
+    localStorage.setItem("token", x.token)
+  }
+
+}
