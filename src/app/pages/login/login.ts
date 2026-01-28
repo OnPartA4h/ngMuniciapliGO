657afc56b26a1932784fc481367e1b0c83647f76
Adan Riasat <6194262@cegepmontpetit.ca>
2026-01-28 16:37:56 -0500

    FCT ReactiveForms pour login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,22 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth-service';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  
+  formGroup: FormGroup;
 
-  email: string = ""
-  password: string = ""
-
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, private formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required]
+      }
+    )
+  }
 
   async login() {
-    await this.authService.login(this.email, this.password)
+    if (!this.formGroup.valid) return
+    let email = this.formGroup.get('email')?.value
+    let password = this.formGroup.get('password')?.value
+
+    await this.authService.login(email, password)
   }
 }
