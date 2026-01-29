64795eca6091198890cecee8d168264ad7c5a477
Adan Riasat <6194262@cegepmontpetit.ca>
2026-01-29 14:15:56 -0500

    FCT List des problèmes signalé

---
+++ src/app/services/white-service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { Problem } from '../models/problem';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WhiteService {
+  domain = "https://localhost:7288"
+  //domain = "https://municipaligo.onrender.com"
+
+  constructor (public http: HttpClient) {}
+
+  async getAllProblems(): Promise<Problem[]> {
+    let x = await lastValueFrom(this.http.get<Problem[]>(this.domain + "/api/ColBlanc/problems"))
+    console.log(x);
+
+    return x
+  }
+}
