d9a99303382d0c85e5d7bdcb4895eb113292eead
hugolevacher <hugolevacher13@gmail.com>
2026-01-30 11:29:29 -0500

    DELETE: Footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { Header } from './components/header/header';
-import { Footer } from './components/footer/footer';
 import { CommonModule } from '@angular/common';
 import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header, Footer, CommonModule],
+  imports: [RouterOutlet, Header, CommonModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
