efc96d480eff5d9f430cf4812290f0d0ec48264b
hugolevacher <hugolevacher13@gmail.com>
2026-02-04 15:46:35 -0500

    FIX: Environment url dans white service

--- src/app/services/white-service.ts
+++ src/app/services/white-service.ts
@@ -2,47 +2,47 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { Problem } from '../models/problem';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WhiteService {
-  domain = "https://localhost:7288"
-  //domain = "https://municipaligo.onrender.com"
+  private apiUrl = environment.apiUrl;
 
   constructor(public http: HttpClient) { }
 
   async getAllProblems(): Promise<Problem[]> {
-    let x = await lastValueFrom(this.http.get<Problem[]>(this.domain + "/api/ColBlanc/problems"))
+    let x = await lastValueFrom(this.http.get<Problem[]>(`${this.apiUrl}/api/ColBlanc/problems`))
     return x
   }
 
   async acceptProblem(id: number): Promise<any> {
     return await lastValueFrom(
-      this.http.post<any>(`${this.domain}/api/ColBlanc/${id}/accepter`, null)
+      this.http.post<any>(`${this.apiUrl}/api/ColBlanc/${id}/accepter`, null)
     );
   }
 
   async refuseProblem(id: number): Promise<any> {
     return await lastValueFrom(
-      this.http.post<any>(`${this.domain}/api/ColBlanc/${id}/refuser`, null)
+      this.http.post<any>(`${this.apiUrl}/api/ColBlanc/${id}/refuser`, null)
     );
   }
 
   async assignProblemCitoyen(id: number): Promise<any> {
     return await lastValueFrom(
-      this.http.post<any>(`${this.domain}/api/ColBlanc/${id}/assign-citoyens`, null)
+      this.http.post<any>(`${this.apiUrl}/api/ColBlanc/${id}/assign-citoyens`, null)
     );
   }
 
   async assignProblemColbleu(id: number, colBleuId: string) {
     return await lastValueFrom(
-      this.http.post<any>(`${this.domain}/api/ColBlanc/${id}/assign-colbleu/${colBleuId}`, null)
+      this.http.post<any>(`${this.apiUrl}/api/ColBlanc/${id}/assign-colbleu/${colBleuId}`, null)
     );
   }
 
   async getProblem(id: number): Promise<any> {
-    let x = await lastValueFrom(this.http.get<Problem>(this.domain + "/api/ColBlanc/problems/" + id))
+    let x = await lastValueFrom(this.http.get<Problem>(`${this.apiUrl}/api/ColBlanc/problems/${id}`))
     return x
   }
 }
