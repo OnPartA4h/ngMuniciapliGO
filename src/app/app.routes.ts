3129138d0a62a0b3fd9850d440d31de14b70da5d
hugolevacher <hugolevacher13@gmail.com>
2026-02-05 12:47:28 -0500

    FIX: Default route to home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -23,6 +23,6 @@ export const routes: Routes = [
   { path: 'report-details/:id', component: ReportDetails, canActivate: [authGuard]},
   { path: 'profile', component: Profile, canActivate: [authGuard]},
   { path: 'landing', component: Landing,},
-  { path: '', redirectTo: '/landing', pathMatch: 'full' },
-  { path: '**', redirectTo: '/landing' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' }
 ];
