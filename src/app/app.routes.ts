cc54567c1324a2d6b9e27c89d8073536f5771fb7
hugolevacher <hugolevacher13@gmail.com>
2026-02-05 12:50:54 -0500

    FIX: Redirect url

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -23,6 +23,6 @@ export const routes: Routes = [
   { path: 'report-details/:id', component: ReportDetails, canActivate: [authGuard]},
   { path: 'profile', component: Profile, canActivate: [authGuard]},
   { path: 'landing', component: Landing,},
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', redirectTo: '/home' }
+  { path: '', redirectTo: '/manage-reports', pathMatch: 'full' },
+  { path: '**', redirectTo: '/manage-reports' }
 ];
