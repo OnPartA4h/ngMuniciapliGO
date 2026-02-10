7f2083945365540e3c9cd9eeeb8aa174479f3c3d
hugolevacher <hugolevacher13@gmail.com>
2026-02-10 12:41:27 -0500

    FCT: Profile info form componenent

---
+++ src/app/components/profile-info-form/profile-info-form.ts
@@ -0,0 +1,72 @@
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { UpdateUserDto, User } from '../../models/user';
+import { UserFormComponent } from '../user-form/user-form';
+
+@Component({
+  selector: 'app-profile-info-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, TranslateModule, UserFormComponent],
+  templateUrl: './profile-info-form.html',
+})
+export class ProfileInfoFormComponent implements OnChanges {
+  @Input() profile: User | null = null;
+  @Input() isSaving = false;
+  @Input() successMessage: string | null = null;
+  @Input() errorMessage: string | null = null;
+
+  @Output() formSubmit = new EventEmitter<UpdateUserDto>();
+
+  profileForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.profileForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+      lastName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+      phoneNumber: ['', [Validators.required, Validators.minLength(10)]],
+      streetNumber: ['', [Validators.required, Validators.maxLength(20)]],
+      streetName: ['', [Validators.required, Validators.maxLength(200)]],
+      city: ['', [Validators.required, Validators.maxLength(100)]],
+      province: ['', [Validators.required, Validators.maxLength(100)]],
+      postalCode: ['', [Validators.required, Validators.maxLength(10), Validators.pattern(/^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/)]]
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['profile'] && this.profile) {
+      this.profileForm.patchValue({
+        firstName: this.profile.firstName,
+        lastName: this.profile.lastName,
+        phoneNumber: this.profile.phoneNumber,
+        streetNumber: this.profile.streetNumber,
+        streetName: this.profile.streetName,
+        city: this.profile.city,
+        province: this.profile.province,
+        postalCode: this.profile.postalCode
+      });
+    }
+  }
+
+  submitForm() {
+    this.profileForm.markAllAsTouched();
+
+    if (this.profileForm.invalid) {
+      return;
+    }
+
+    const dto: UpdateUserDto = {
+      firstName: this.profileForm.value.firstName,
+      lastName: this.profileForm.value.lastName,
+      phoneNumber: this.profileForm.value.phoneNumber,
+      streetNumber: this.profileForm.value.streetNumber,
+      streetName: this.profileForm.value.streetName,
+      city: this.profileForm.value.city,
+      province: this.profileForm.value.province,
+      postalCode: this.profileForm.value.postalCode
+    };
+
+    this.formSubmit.emit(dto);
+  }
+}
