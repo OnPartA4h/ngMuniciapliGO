4ab1f95b3c549e07e22706e6048a19c64af32267
hugolevacher <hugolevacher13@gmail.com>
2026-02-10 12:40:04 -0500

    FCT: Password modal component

---
+++ src/app/components/password-success-modal/password-success-modal.ts
@@ -0,0 +1,33 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-password-success-modal',
+  standalone: true,
+  imports: [CommonModule, TranslateModule],
+  templateUrl: './password-success-modal.html',
+  styleUrl: './password-success-modal.css',
+})
+export class PasswordSuccessModalComponent {
+  @Input() generatedPassword: string | null = null;
+  @Input() isOpen = false;
+  @Output() close = new EventEmitter<void>();
+
+  constructor(private translateService: TranslateService) {}
+
+  async copyPassword() {
+    if (this.generatedPassword) {
+      try {
+        await navigator.clipboard.writeText(this.generatedPassword);
+        alert(this.translateService.instant('CREATE_USER.PASSWORD_COPIED'));
+      } catch (err) {
+        console.error('Failed to copy password:', err);
+      }
+    }
+  }
+
+  closeModal() {
+    this.close.emit();
+  }
+}
