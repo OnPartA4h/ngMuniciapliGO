cef3050398a46d8ec723ab38298e9fe282c21e1b
hugolevacher <hugolevacher13@gmail.com>
2026-02-16 14:28:16 -0500

    UPDATE: Nouveau champ dans le modèle

--- src/app/models/problem.ts
+++ src/app/models/problem.ts
@@ -38,7 +38,8 @@ export class Problem {
     public photos: Photo[],
     public resolutionPhotos: Photo[],
     public resolutionDescription: string | null,
-    public rejectionReason: string | null
+    public rejectionReason: string | null,
+    public isInOpenDuplicateGroup: boolean
   ) { }
 }
 
