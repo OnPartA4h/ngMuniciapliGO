cfd038991143cbb349449af51a24420c687094a4
hugolevacher <hugolevacher13@gmail.com>
2026-02-27 09:54:44 -0500

    FIX: Traduction, btn-create style et scrollbar edit user modal

--- src/app/components/ui/toast/toast.ts
+++ src/app/components/ui/toast/toast.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { ToastService, Toast } from './toast.service';
+import { ToastService } from './toast.service';
 
 @Component({
   selector: 'app-toast',
