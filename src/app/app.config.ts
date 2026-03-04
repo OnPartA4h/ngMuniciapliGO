b03105e0655414c3b82f76387a82886b3e0ce466
HenriSaumure <169080985+HenriSaumure@users.noreply.github.com>
2026-03-04 10:43:37 -0500

    ._.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -34,5 +34,5 @@ export const appConfig: ApplicationConfig = {
   ]
 };
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
 }
